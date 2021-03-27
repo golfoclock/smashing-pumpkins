c89a388e22e123f1b3bbe5a7a1d1d16e371d1873
golfoclock <cmmc@protonmail.ch>
2021-03-26 23:09:03 -0400

    add labels on x and y axes

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const data = [
     { record: 'Gish', amount: 1000000 },
     { record: 'Pieces Iscariot', amount: 1000000},
     { record: 'Siamese Dream', amount: 4000000 },
-    { record: 'Mellon Collie and the Infinite Sadness', amount: 10000000 },
+    { record: 'Mellon Collie ...', amount: 10000000 },
     { record: 'Adore', amount: 1000000 },
     { record: 'MACHINA', amount: 5000000 },
     { record: 'Zeitgeist', amount: 5000000 },
    
   ];
 
-const width = 800;
+const width = 1400;
 const height = 600;
 const margin = { top: 50, bottom: 50, left: 50, right: 50};
 
@@ -28,6 +28,7 @@ const y = d3.scaleLinear()
     .domain([0, 10000000])
     .range([height - margin.bottom, margin.top])
 
+
 svg 
     .append('g')
     .attr('fill', 'royalblue')
@@ -38,5 +39,20 @@ svg
         .attr('y', (d) => y(d.amount))
         .attr('height', d => y(0) - y(d.amount))
         .attr("width", x.bandwidth());
+
+function yAxis(g) {
+  g.attr("transform", `translate(${margin.left}, 0)`)
+    .call(d3.axisLeft(y).ticks(null, data.format))
+    .attr("font-size", '20px')
+}
+
+function xAxis(g) {
+  g.attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).tickFormat(i => data[i].record))
+    .attr("font-size", '16px')
+}
+
+svg.append("g").call(xAxis);
+svg.append("g").call(yAxis);
 
 svg.node();
